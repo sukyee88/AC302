78d258c3b2d8d8121f9198de81056e1b1ccd7e67
sukyee88 <sukyee.gong@gmail.com>
2018-11-08 13:08:50 +0800

    Phaser0811

--- phaser/app.js
+++ phaser/app.js
@@ -31,63 +31,69 @@ ledge2.body.immovable = true;
 //set text style
 var style = {font: "bold 32px Arial", fill:"#fff"};
 
-scorelabel = game.add.text(700, 5, "Score:", style);
+scorelabel = game.add.text(650, 5, "Score:", style);
 scorenumber = game.add.text(760,5, score, style);
 
 lifelabel = game.add.text(10,5, "Lives:", style);
 lifenumber = game.add.text(100,5, lives, style);
 
-//Player settings
-player = game.add.sprite(32,400, 'player');
+player = game.add.sprite(32, 400, 'dude');
+		// Animating the player sprite
+		player.animations.add('left',[0, 1, 2, 3],10,true);
+		player.animations.add('right',[5, 6, 7, 8],10,true);
+		game.physics.arcade.enable(player);
+		player.body.bounce.y = 0.2;
+		player.body.gravity.y = 300;
+		player.body.collideWorldBounds = true;
 
-player.animations.add('left', [0,1,2,3],10,true);
-player.animations.add('right', [5,6,7,8],10,true);
-game.physics.arcade.enable(player);
-player.body.bounce.y = 0.2;
-player.body.gravity.y = 300;
-player.body.collideWorldBounds = true;
+	// Create the enemy
+  	enemy1 = game.add.sprite(760, 20, 'baddie');
+    // Animate the enemy1
+    	enemy1.animations.add('left', [0,1], 10, true);
+    	enemy1.animations.add('right', [2,3], 10, true);
+    	game.physics.arcade.enable(enemy1);
+    	enemy1.body.bounce.y = 0.2;
+    	enemy1.body.gravity.y = 500;
+    	enemy1.body.collideWorldBounds = true;
 
-//Enemy settings
-enemy = game.add.sprite(760,20, 'enemy');
+    // Create the stars
+	stars = game.add.physicsGroup();
+	stars.enableBody = true;
+	// We will create 12 stars evenly spaced
+	for(var i = 0; i < 12; i++){
+		var star = stars.create(i * 70, 0, 'star');
+		star.body.gravity.y = 200;
+		star.body.bounce.y = 0.7 + Math.random() * 0.2;
+	}
 
-enemy.animations.add('left', [0,1], 10, true);
-enemy.animations.add('right', [2,3], 10, true);
-game.physics.arcade.enable(enemy);
-enemy.body.bounce.y = 0.2;
-enemy.body.gravity.y = 500;
-enemy.body.collideWorldBounds = true;
+	// Create keyboard entries
+	cursors = game.input.keyboard.createCursorKeys();
 
-//Create the stars
-stars = game.add.physicsGroup();
-stars.enableBody = true;
-
-for (var i = 0; i < 12; i++) {
-	var star = stars.create(i * 70, 0 , 'star');
-	star.body.gravity.y = 200;
-	star.body.bounce.y = 0.7 + Math.random() *0.2;
-
-}
-cursors = game.input.keyboard.createCursorKeys();
 }
 
 function update(){
 	game.physics.arcade.collide(player, platforms);
-	game.physics.arcade.collide(enemy, platforms);
 	game.physics.arcade.collide(stars, platforms);
+	game.physics.arcade.collide(enemy1, platforms);
 
+	//reset the player's velocity if no events.
 	player.body.velocity.x = 0;
 
-	if (cursors.left.isDown){
-		player.body.velocity.x = -100;
+	//player movement by keys
+	if(cursors.left.isDown){
+		//move left
+		player.body.velocity.x = -150;
 		player.animations.play('left');
-	}else if (cursors.right.isDown){
-	player.body.velocity.x = 100;
+	} else if(cursors.right.isDown){
+		//move right
+		player.body.velocity.x = 150;
 		player.animations.play('right');
-	}else{
+	} else {
 		player.animations.stop();
 		player.frame = 4;
 	}
 
+	//allow the player to jump if touching the ground
 	if(cursors.up.isDown && player.body.touching.down){
 		player.body.velocity.y = -300;
 	}
